144d499e51cc70a251309d081a7341f24ecb195b
Andrian <132529758+ABRestauro@users.noreply.github.com>
2024-04-25 10:12:48 +0800

    1st commit database

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 #note: comment out columns that are useful for later
+#note: the difference between now_add and auto_now is that data from now_add cannot be change after it was added while the auto_now will change everytime it is updated
 class Room(models.Model):
     #host=
     #topic=
@@ -10,4 +11,4 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     #participants=
     updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)  #the difference between now_add and auto_now is that data from now_add cannot be change after it was added while the auto_now will change everytime it is updated
+    created = models.DateTimeField(auto_now_add=True)  
